c2955010a294258d805273124f432e0922aeb372
Matt McQuinn <mmcquinn77@gmail.com>
2017-04-26 01:06:50 -0500

    Configure adapter to use the host according to the environment

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -1,6 +1,8 @@
 import DS from 'ember-data';
 import DataAdapterMixin from 'ember-simple-auth/mixins/data-adapter-mixin';
+import ENV from "../config/environment";
 
 export default DS.JSONAPIAdapter.extend(DataAdapterMixin, {
-  authorizer: 'authorizer:jwt'
+  authorizer: 'authorizer:jwt',
+  host: ENV.host
 });
