8aa8d030935cb6a38ac11782a0c8cec4f1420236
Matt McQuinn <mmcquinn77@gmail.com>
2017-04-26 04:49:45 -0500

    Add password field to users

--- app/models/user.js
+++ app/models/user.js
@@ -3,5 +3,6 @@ import DS from 'ember-data';
 export default DS.Model.extend({
   name: DS.attr('string'),
   email: DS.attr('string'),
+  password: DS.attr('string'),
   rounds: DS.hasMany('round')
 });
