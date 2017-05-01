428b9f54ef195634044e0fb9ffe57e68c29d3d09
Matthew McQuinn <mmcquinn77@gmail.com>
2017-05-01 16:48:29 -0500

    Display a rounds score
    Add a rounds score to the template and model

--- app/models/round.js
+++ app/models/round.js
@@ -2,6 +2,7 @@ import DS from 'ember-data';
 
 export default DS.Model.extend({
   weather: DS.attr('string'),
+  score: DS.attr('number'),
   user: DS.belongsTo('user'),
   course: DS.belongsTo('course'),
   scores: DS.hasMany('scores')
